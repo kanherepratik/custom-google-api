cde8ce84ec282ae8da6ab20b3ce1d268b4c4dd42
Prateek Kanhere <prateek.kanhere@coverfoxmail.com>
2019-12-18 12:38:06 +0530

    print data from google api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { CUSTOM_SEARCH_LINK } from "./constants";
+import Table from "./Table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      data: [],
+      searchText: ""
+    };
+  }
+
+  getGoogleSearchData = link => {
+    // set loading to true
+    this.setState({ isLoading: true });
+    // get results in json format
+    axios.get(link).then(response => {
+      this.setState({ data: response.data.items, isLoading: false });
+    });
+  };
+
+  handleChange = event => {
+    this.setState({ searchText: event.target.value });
+  };
+  onSearch = () => {
+    // appending search query
+    const finalLink = CUSTOM_SEARCH_LINK + this.state.searchText;
+    this.getGoogleSearchData(finalLink);
+  };
+
+  render() {
+    const columns = [
+      {
+        Header: "Google Results",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "title"
+          },
+          {
+            Header: "Link",
+            accessor: "link"
+          }
+        ]
+      }
+    ];
+
+    return (
+      <>
+        <div>
+          <input
+            type="text"
+            value={this.state.searchText}
+            onChange={this.handleChange}
+          />
+          <button type="submit" onClick={this.onSearch}>
+            Search
+          </button>
+        </div>
+        {this.state.isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <Table columns={columns} data={this.state.data} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const CUSTOM_SEARCH_LINK =
+  "https://www.googleapis.com/customsearch/v1?key=AIzaSyBswZCr4C4cC88Dt1cVZZuVR4ax9qqynns&cx=008448813968373854426:sbsnaikeh39&q=";
